1860e474c806634ae8672d980d6e00a772e71606
BelowZeronb <129643055+BelowZeronb@users.noreply.github.com>
2024-08-13 19:55:31 +0800

    Update ICE-cfm.sh

--- ICE-cfm.sh
+++ ICE-cfm.sh
@@ -2,67 +2,6 @@
 #清理
 echo -e "\033[44;37m 欢迎使用ICE-cfm \033[0m"
 sleep 0.5
-#微验Shell模板
-if ! [ -e "rc4" ]; then
-    echo "rc4文件缺失"
-    exit 1
-fi
-chmod 777 rc4
-
-#配置区
-wyUrl="http://wy.llua.cn/api/" #API接口(一般不用改)
-wyAppid="66424" #APPID(应用ID)
-wyAppkey="iP7pxOT725fz2FOO" #APPKEY(应用秘钥)
-wyRc4key="Fdzkxb5eeR2l2" #Rc4KEY(Rc4秘钥)
-deCode="994" #code(登录自定义状态码)
-
-
-#函数区
-parse_json() {
-  json=$1
-  query=$2
-  value=$(echo "$json" | grep -o "\"$query\":[^ }]*" | sed 's/"[^"]*":\([^,}]*\).*/\1/' | head -n 1)
-  value="${value#\"}"
-  value="${value%\"}"
-  echo "$value"
-}
-
-#公告区
-notice=`curl -s "${wyUrl}?id=notice&app=${wyAppid}"`
-deNotice=`./rc4 $notice $wyRc4key "de"`
-Notices=`parse_json "$deNotice" "app_gg"`
-echo "系统公告:\n${Notices}\n"
-
-#验证区
-echo "请输入卡密：(点击屏幕右下角lm弹窗键盘)"
-read kami
-timer=`date +%s`
-android_id=`settings get secure android_id`
-fingerprint=`getprop ro.build.fingerprint`
-imei=`echo -n "${android_id}.${fingerprint}" | md5sum | awk '{print $1}'`
-value="$RANDOM${timer}"
-sign=`echo -n "kami=${kami}&markcode=${imei}&t=${timer}&${wyAppkey}" | md5sum | awk '{print $1}'`
-data=`./rc4 "kami=${kami}&markcode=${imei}&t=${timer}&sign=${sign}&value=${value}&${wyAppkey}" $wyRc4key "en"`
-logon=`curl -s "${wyUrl}?id=kmlogon&app=${wyAppid}&data=${data}"`
-deLogon=`./rc4 $logon $wyRc4key "de"`
-wy_Code=`parse_json "$deLogon" "code"`
-if  [ "$wy_Code" -eq "$deCode" ]; then
-    timec=`parse_json "$deLogon" "time"`
-    check=`echo -n  "${timec}${wyAppkey}${value}" | md5sum | awk '{print $1}'`
-    checks=`parse_json "$deLogon" "check"`
-    if  [ "$check" == "$checks" ]; then
-        vip=`parse_json "$deLogon" "vip"`
-        vips=`date -d @$vip +"%Y-%m-%d %H:%M:%S"`
-        clear
-        echo "登录成功，到期时间：${vips}"
-    else
-        echo "校验失败"
-        exit
-    fi
-else
-    parse_json "$deLogon" "msg"
-    exit
-fi
 #主体
 echo "验证成功后程序开始执行..."
 echo -e "\033[46;30m 企鹅群:976998338，tg群:@ICENbNb \033[0m"
